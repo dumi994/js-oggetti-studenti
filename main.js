54dbf6e2235406c8eeb138a16ce26fbf774af9fe
dumi994 <dumi.9944@gmail.com>
2021-05-07 16:30:40 +0200

    inserito oggetto studente

---
+++ main.js
@@ -0,0 +1,17 @@
+//Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome età.
+var studente = {
+    'nome': 'paolo',
+    cognome: 'rossi',
+    eta: 2021 - 1993,  
+}
+console.log(studente);
+//Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+
+
+//Creare un array di oggetti di studenti.
+
+
+//Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+
+
+//Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
