ea3efdb01a7a5f9b41f5f8628fa779d40b6d5788
dumi994 <dumi.9944@gmail.com>
2021-05-07 17:25:44 +0200

    creato array studenti

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var studente = {
     cognome: 'rossi',
     eta: 2021 - 1993,  
 }
+
 var key;
 var testo = '';
 console.log(studente);
@@ -12,12 +13,62 @@ console.log(studente);
 for (key in studente){
     console.log(studente[key]);
     testo += studente[key] + ' ';
-    document.getElementById('nomi').innerHTML = testo;
 }
-/* document.getElementById('nomi').innerHTML = studente[key]; */
+document.getElementById('nomi').innerHTML = testo;
 
 //Creare un array di oggetti di studenti.
+/* testo += studente[key] + ' ';
+    document.getElementById('nomi').innerHTML = testo */;
 
+/* var studenti = [
+    
+    
+    {
+        'nome': 'mario',
+        cognome: 'draghi',
+        eta: 2021 - 1947,
+    },
+    {
+        'nome': 'matteo',
+        cognome: 'renzie',
+        eta: 2021 - 1975,
+    },
+    {
+        'nome': 'Matteo',
+        cognome: 'Renzie',
+        eta: 2021 - 1975,
+    },
+    {
+        'nome': 'Beppe',
+        cognome: 'Fiorello',
+        eta: 2021 - 1969,
+    },
+    document.getElementById('nomi').innerHTML = testo 
+ 
+]
+ */
+var studenti = [
+    studente1 = {
+        'nome': 'mario',
+        cognome: 'draghi',
+        eta: 2021 - 1947,
+    },
+    studente2 = {
+        'nome': 'matteo',
+        cognome: 'renzie',
+        eta: 2021 - 1975,
+    },
+    studente3 = {
+        'nome': 'Beppe',
+        cognome: 'Fiorello',
+        eta: 2021 - 1969,
+    }
+  ];
+  console.log(studenti);
+  
+ 
+  
+console.log(studenti);
 
 //Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
 
